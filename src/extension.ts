0fb4eb1b90f302a1afd233d0338f5f5c862ebf9b
TanHui <915156161@qq.com>
2021-04-26 15:35:06 +0800

    feat: 支持在vscode中自定义格式化选项

--- src/extension.ts
+++ src/extension.ts
@@ -2,45 +2,89 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 import * as hjson from 'hjson';
+
+let customOptions: HjsonOptions = {};
+
+const defaultOptions: HjsonOptions = {
+  separator: true,
+  condense: 4,
+  bracesSameLine: true,
+  quotes: 'keys',
+  space: 2
+};
+
+function getOptions(): void {
+  let config = vscode.workspace.getConfiguration('hjson-formatter');
+  customOptions = config.get<HjsonOptions>('options', {});
+}
+
+vscode.workspace.onDidChangeConfiguration(getOptions);
+
+getOptions();
+
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+  // Use the console to output diagnostic information (console.log) and errors (console.error)
+  // This line of code will only be executed once when your extension is activated
+  console.log(
+    'Congratulations, your extension "hjson-formatter" is now active!'
+  );
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "hjson-formatter" is now active!');
+  // The command has been defined in the package.json file
+  // Now provide the implementation of the command with registerCommand
+  // The commandId parameter must match the command field in package.json
+  const format = (
+    document: vscode.TextDocument,
+    options: vscode.FormattingOptions
+  ) => {
+    const formatOptions = {
+      ...defaultOptions,
+      ...customOptions
+    };
+    // set default eol by current document
+    const documentEol = document.eol === vscode.EndOfLine.LF ? '\n' : '\r\n';
+    formatOptions.eol =
+      !formatOptions.eol || formatOptions.eol === 'auto'
+        ? documentEol
+        : formatOptions.eol;
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const format = (document: vscode.TextDocument) => {
-		const result: vscode.TextEdit[] = [];
+    // set indents size
+    formatOptions.space =
+      !formatOptions.space || formatOptions.space === 'auto'
+        ? options.tabSize
+        : formatOptions.space;
 
-		const start = new vscode.Position(0, 0);
-		const end = new vscode.Position(document.lineCount - 1, document.lineAt(document.lineCount - 1).text.length);
-		const range = new vscode.Range(start, end);
-		const s = document.getText(range);
-		try {
-			const parsed = hjson.rt.parse(s);
-			const formattedText = hjson.rt.stringify(parsed, {
-				separator: true, // 元素之间使用逗号分隔符
-				condense: 4, // 最小换行长度，超出长度后将换行， 0：off
-				bracesSameLine: true, // 括号是否保持跟key同行
-				quotes: 'keys' // 控制字符串如何展示，是否要包裹引号。keys: key需要使用引号包裹
-			});
-			result.push(new vscode.TextEdit(range, formattedText));
-			return result;
-		} catch (e) {
-			vscode.window.showErrorMessage("Formatted document failed, please check your syntax");
-			console.error(e);
-		}
-	};
-	context.subscriptions.push(vscode.languages.registerDocumentFormattingEditProvider('hjson', {
-		provideDocumentFormattingEdits(document, options, token) {
-			return format(document);
-		}
-	}));
+    const result: vscode.TextEdit[] = [];
+
+    const start = new vscode.Position(0, 0);
+    const end = new vscode.Position(
+      document.lineCount - 1,
+      document.lineAt(document.lineCount - 1).text.length
+    );
+    const range = new vscode.Range(start, end);
+    const s = document.getText(range);
+    try {
+      hjson.setEndOfLine(formatOptions.eol);
+      const parsed = hjson.rt.parse(s);
+      const formattedText = hjson.rt.stringify(parsed, formatOptions);
+      result.push(new vscode.TextEdit(range, formattedText));
+      return result;
+    } catch (e) {
+      vscode.window.showErrorMessage(
+        'Formatted document failed, please check your syntax'
+      );
+      console.error(e);
+    }
+  };
+  context.subscriptions.push(
+    vscode.languages.registerDocumentFormattingEditProvider('hjson', {
+      provideDocumentFormattingEdits(document, options, token) {
+        return format(document, options);
+      }
+    })
+  );
 }
 
 // this method is called when your extension is deactivated
-export function deactivate() { }
+export function deactivate() {}
--- typings/index.d.ts
+++ typings/index.d.ts
@@ -2,11 +2,25 @@
  * @Author: TanHui
  * @Date: 2021-02-23 11:44:01
  * @LastEditors: TanHui
- * @LastEditTime: 2021-02-23 14:36:59
+ * @LastEditTime: 2021-04-26 14:31:52
  * @Description:
  */
+declare interface HjsonOptions {
+    condense?: number;
+    bracesSameLine?: boolean;
+    emitRootBraces?: boolean;
+    quotes?: 'min' | 'keys' | 'strings' | 'all';
+    multiline?: "std" | "no-tabs" | "off";
+    separator?: boolean;
+    space?: number | string;
+    eol?: string;
+    colors?: boolean;
+    serializeDeterministically?: boolean
+}
+
 declare module "hjson" {
     interface HJSON {
+        setEndOfLine: (eol: string) => void;
         rt: {
             parse: (text: string, options?: object) => any,
             stringify: (source: any, options?: object) => string
